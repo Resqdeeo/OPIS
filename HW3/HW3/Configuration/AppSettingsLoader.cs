14f959ea4d4b901c92ab874080578ce7a348b08a
Valery Lazarev <112940499+Resqdeeo@users.noreply.github.com>
2023-12-24 22:24:24 +0300

    HW3

---
+++ HW3/HW3/Configuration/AppSettingsLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HW3.Configuration
+{
+    public class AppSettingsLoader
+    {
+        public string Path { get; private set; }
+        public string CurrentDirectory { get; private set; }
+        public AppSettingsClass? Configuration { get; private set; }
+
+        public AppSettingsLoader(string currentDirectory)
+        {
+            CurrentDirectory = currentDirectory;
+            Path = CurrentDirectory + "appsettings.json";
+        }
+
+        public void InitializeAppSettings()
+        {
+            try
+            {
+                using var sr = new StreamReader(Path);
+                var json = sr.ReadToEnd();
+                Configuration = JsonSerializer.Deserialize<AppSettingsClass>(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            if (Configuration is null)
+                throw new ArgumentException("appsettings.json not found");
+        }
+    }
+}
