cda2e8a5a4ffff3077d816b3727546637d802cdf
Valery Lazarev <112940499+Resqdeeo@users.noreply.github.com>
2023-12-24 22:16:05 +0300

    HW3

---
+++ HW4/Homeworktasks/Handlers/StaticFilesHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworktasks.Handlers
+{
+    public class StaticFilesHandler
+    {
+        private string StaticFolder { get; set; }
+
+        public StaticFilesHandler(string staticFolderPath)
+        {
+            if (Directory.Exists(staticFolderPath))
+                StaticFolder = staticFolderPath;
+            else
+                Directory.CreateDirectory(staticFolderPath);
+        }
+
+        public byte[] fetchStaticFile(string fileName)
+        {
+            return File.Exists(StaticFolder + "/" + fileName)
+                ? File.ReadAllBytes(StaticFolder + "/" + fileName)
+                : Array.Empty<byte>();
+        }
+    }
+}
