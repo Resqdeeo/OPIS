265a79502cfe91926a619c5d29fb469fca3d0ab9
Valery Lazarev <112940499+Resqdeeo@users.noreply.github.com>
2024-01-08 14:43:41 +0300

    HW8

---
+++ HW8/Homeworktasks/Attributes/GetAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworktasks.Attributes
+{
+    internal class GetAttribute : HttpMethodAttribute
+    {
+        public GetAttribute(string actionName) : base(actionName)
+        { }
+    }
+}
---
+++ HW8/Homeworktasks/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworktasks.Models
+{
+    public class Account
+    {
+        public uint Id { get; init; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
