2c4e35812bd30ff1b6e80ea068635b9fcea9d354
Valery Lazarev <112940499+Resqdeeo@users.noreply.github.com>
2024-01-08 15:18:30 +0300

    HW10

---
+++ Shablonizator/Shablonizator/Models/StudentEx1.cs
@@ -0,0 +1,7 @@
+﻿namespace Shablonizator.Models;
+
+public class StudentEx1
+{
+    public string Surname { get; set; } = "Lazarev";
+    public string Name { get; set; } = "Valeriy";
+}
---
+++ Shablonizator/Shablonizator/Program.cs
@@ -0,0 +1,17 @@
+﻿using Shablonizator;
+using Shablonizator.Models;
+
+Console.WriteLine(PhrasesToStudents.Ex1("Лейсан"));
+Console.WriteLine();
+Console.WriteLine(PhrasesToStudents.Ex2(new StudentEx2 { Address = "Ул.Пушкина" }));
+Console.WriteLine();
+Console.WriteLine(PhrasesToStudents.Ex3(new StudentEx3()));
+Console.WriteLine();
+
+var table = new Table();
+table.Students.Add(new StudentForTable { Fio = "Александр Никифоров", Grade = 0 });
+table.Students.Add(new StudentForTable { Fio = "Антон Никулин", Grade = 100 });
+table.Students.Add(new StudentForTable { Fio = "Петр Николаев", Grade = 71 });
+table.Students.Add(new StudentForTable { Fio = "Даниил Тетерин", Grade = 56 });
+
+Console.WriteLine(PhrasesToStudents.Ex4(table));
